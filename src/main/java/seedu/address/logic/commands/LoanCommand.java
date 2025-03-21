c2f4b95b2bb7318d8a771313d89926b3ff38740e
wenruu <e1122418@u.nus.edu>
2025-03-22 01:17:42 +0800

    Minor changes

--- src/main/java/seedu/address/logic/commands/LoanCommand.java
+++ src/main/java/seedu/address/logic/commands/LoanCommand.java
@@ -4,8 +4,12 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+import java.util.List;
 
 public class LoanCommand extends Command {
 
@@ -43,6 +47,14 @@ public class LoanCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToAddLoan = lastShownList.get(index.getZeroBased());
         return null;
     }
 }
