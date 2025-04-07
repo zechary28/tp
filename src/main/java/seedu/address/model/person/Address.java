484c7dd809b8146232e47fd6ca024da25a569cff
Zechary <zechary28@gmail.com>
2025-04-08 04:14:54 +0800

    Fix character length

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -15,7 +15,7 @@ public class Address {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[^\\s].{0,70}";
+    public static final String VALIDATION_REGEX = "[^\\s].{0,69}";
 
     public final String value;
 
